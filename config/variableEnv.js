ef85fdb437d7d069a0636e1386eebbbe56ad23cb
afriansyah husni <afriansyah@Brians-MacBook-Pro.local>
2019-09-25 15:17:14 +0700

    fix error

--- config/variableEnv.js
+++ config/variableEnv.js
@@ -3,5 +3,5 @@ require("dotenv").config();
 module.exports = {
   MONGOOSE_DB: process.env.MONGOOSE_DB,
   PORT: process.env.PORT,
-  MOONGOSE_CONNECTION: process.env.MONGOOSE_CONNECTION
+  MONGOOSE_CONNECTION: process.env.MONGOOSE_CONNECTION
 };
